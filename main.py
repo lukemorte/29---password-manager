0e4a05abc5be5e0e950598b981669893d7c85c3a
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-30 16:12:51 +0200

    add logo image and implement basic UI setup in main.py

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-# password manager
+import tkinter
 
+SCREEN_WIDTH = 200
+SCREEN_HEIGHT = 200
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = tkinter.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = tkinter.Canvas(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, bg="white", highlightthickness=0)
+logo_image = tkinter.PhotoImage(file="logo.png")
+
+canvas.create_image(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, image=logo_image, anchor="center")
+
+# zobrazení - teprve v ten moment se objekt vykreslí
+canvas.grid(column=1, row=1)
+
+
+window.mainloop()
