a2f3a551ca3aa18a9a401af7a3f2725c9cc1d4a6
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-30 16:38:00 +0200

    fix: correct variable name for logo image and update canvas layout method

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 canvas = tkinter.Canvas(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, bg="white", highlightthickness=0)
-logo_image = tkinter.PhotoImage(file="logo.png")
+logo_img = tkinter.PhotoImage(file="logo.png")
 
-canvas.create_image(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, image=logo_image, anchor="center")
+canvas.create_image(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, image=logo_img, anchor="center")
 
 # zobrazení - teprve v ten moment se objekt vykreslí
-canvas.grid(column=1, row=1)
+canvas.pack()
 
 
 window.mainloop()
